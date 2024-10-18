a3f4f337003bc070780d608db45e93c81644837c
hashem202020 <75537854+hashem202020@users.noreply.github.com>
2024-10-18 15:13:47 +0300

    Initial Commit

---
+++ Rick and Morty/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rick and Morty
+//
+//  Created by Khaled hashem on 18/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
